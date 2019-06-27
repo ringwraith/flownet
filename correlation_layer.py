a0dcfb567c4d09ed2fb7c2767b337cc204a65161
Hyungon_Ryu <yhgon@users.noreply.github.com>
2019-06-28 07:55:06 +0900

    Delete correlation_layer.py

--- correlation_layer.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-correlation_module = tf.load_op_library("./build/libcorrelation.so")
-
-#Import and register the correltion gradient function
-import _correlation_grad
-
-corr = correlation_module.correlation
